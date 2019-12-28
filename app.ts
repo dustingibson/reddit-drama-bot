d30be0bb5cbf74ab4ebc1baf3e7e64347847d767
dustingibson <dustingibson@outlook.com>
2019-12-28 13:52:17 -0600

    Post to subreddit

--- app.ts
+++ app.ts
@@ -7,8 +7,14 @@ import sqlite3 = require('sqlite3');
 //const bodyParser = require('body-parser');
 import * as fetch from 'node-fetch';
 import { linkSync } from 'fs';
+const snoowrap = require('snoowrap');
+import * as config from './config.json';
+
 
 const threshold = -40;
+const thresholdWithComments = 10;
+const minComments = 30;
+const minCommentsWithThreshold = 10;
 
 let db = new sqlite3.Database('./db/db.bin');
 
@@ -24,29 +30,86 @@ app.get('/', (req, res) => {
   res.send("hello");
 });
 
+app.get('/authtest', (req, res) => {
+  const r = new snoowrap({
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
+    clientId: config.clientid,
+    clientSecret: config.secret,
+    username: config.username,
+    password: config.password
+  });
+  r.getSubreddit('RedditDramaBot').submitLink({
+    title: 'Test',
+    url: 'https://google.com'
+  });
+  res.send("test");
+});
+
+app.post('/postthread', async (req, res) => {
+  const query = "SELECT * FROM DATA WHERE STATUS='RECORDED' AND DATA <>'[removed]' AND DATA <> '[deleted]' ORDER BY RANDOM() DESC LIMIT 1";
+  let data = {};
+  const prom = await new Promise(function (resolve, reject) {
+  db.all(query, function(err, rows) {
+      rows.forEach( async (row) => { 
+        data = row;
+      });
+      resolve(data);
+    })
+  });
+  const updateQuery = `UPDATE DATA SET STATUS='POSTED' WHERE ID=?`;
+  db.run(updateQuery, [data['ID']], function(err) {
+  });
+  const subreddit = getSubredditFromLink(data['LINK']);
+  const comment = data['DATA'].substr(0, Math.min(150, data['DATA'].length));
+  const title = `[${subreddit}] "${comment}..."`;
+  const r = new snoowrap({
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
+    clientId: 'Qe2M9w6z9mXwQg',
+    clientSecret: 'WX7o-hxXYdY-mCNS6C1444R2OLI',
+    username: 'reddit_drama_bot',
+    password: 'readme123'
+  });
+  r.getSubreddit('RedditDramaBot').submitLink({
+    title: title,
+    url: data['LINK']
+  });
+  res.send(title);
+});
+
 app.post('/subreddit',  (req, res) => {
+  try {
   getRedditList(req.query.url).then( (result) => {
        res.send(result);
      });;
-
+  }
+  catch(err) {
+    res.send("ERROR");
+  }
 });
 
 app.post('/reddit',  (req, res) => {
-  traverseSubreddits().then( (result) => {
-       res.send(result);
-     });;
-
+  try {
+    traverseSubreddits().then( (result) => {
+        res.send(result);
+      });;
+  }
+  catch(err) {
+    res.send("ERROR");
+  }
 });
 
-app.listen(3050, () => {
-  console.log('listening on port 3050');
+app.listen(config.port, () => {
+  console.log('listening on port ' + config.port);
 });
+
+function getSubredditFromLink(link) {
+  return link.split('/')[4]
+}
 
  async function traverseSubreddits() {
   let links = [];
     const prom = await new Promise(function (resolve, reject) {
       db.all("SELECT LINK AS link FROM SUBREDDITS WHERE ACTIVE='Y'", function(err, rows) {
-
         rows.forEach( async (row) => { 
           links.push(row.link);
         });
@@ -60,6 +123,10 @@ app.listen(3050, () => {
     }));
     return results;
 }
+
+function naiveCountReplies(dataStr) {
+  return dataStr.split("\"replies\":").length;
+} 
 
 function countReplies(replies, cntObj) {
   replies.forEach((reply) => {
@@ -78,18 +145,27 @@ async function getRedditComments(url) {
     const response = await result.json(url);
     const comments = response[1].data.children;
     comments.forEach( async (comment) => {
-      const score = comment.data.score;
-      if( score <= threshold ) { 
-        links.push( comment.data.permalink );
-        const data = await getCommentData(comment.data.permalink);
-        const replies = data[1].data.children[0].data.replies.data.children;
-        const count = countReplies(replies, {cnt: 0});
-        insertLinkToDB(url, comment.data.permalink, JSON.stringify(data), "RECORDED", score, count);
+      try {
+        if(comment["data"] != undefined && comment.data["permalink"] != undefined) {
+          const score = comment.data.score;
+          if( comment.data.replies != "" && score <= thresholdWithComments && score != 0 && score != 1 )
+          {
+            const data = await getCommentData(comment.data.permalink);
+            const ogComment = comment.data.body;
+            //let count = 0;
+            let count = naiveCountReplies(JSON.stringify(data));
+            if( (score <= threshold && count >= minCommentsWithThreshold) || (score <= thresholdWithComments && count >= minComments) ) { 
+              links.push( comment.data.permalink );
+              insertLinkToDB(url, "https://reddit.com" +  comment.data.permalink, ogComment, "RECORDED", score, count);
+            }
+          }
+        }
+      } catch(err) {
+        console.log("Error getting comments");
       }
     });
-    return links;
+    return "";
   } catch(err) {
-    console.log(results);
     return "";
   }
 }
@@ -98,20 +174,33 @@ async function getCommentData(permaLink) {
   try {
     const url = "https://reddit.com" + permaLink.substr(0, permaLink.length-1) + ".json";
     const result = await fetch(url);
-    const response = await result.json();
-    return response;
+    try {
+      const response = await result.json();
+      return response;
+    } catch(err) {
+      console.log("Response to JSON error");
+      return [];
+    }
   }
   catch(err) {
-    return "{}";
+    console.log("Error retrieving reddit comment JSON");
+    return [];
   }
 }
 
 async function insertLinkToDB(srLink, link, data, status, score, comments) {
+  console.log(comments);
+  let error = false;
   let query = `INSERT OR REPLACE INTO DATA ("LINK", "DATA", "STATUS", "DATALINK", "SCORE", "COMMENTS") VALUES (?, ?, ?, ?, ?, ?)`;
-  const stmt = db.run(query, [link, data, status, srLink, score, comments], function(err) {
-    console.log(err);
+  db.run(query, [link, data, status, srLink, score, comments], function(err) {
+    console.log("Error inserting " + query);
+    error = true;
   });
-  return stmt;
+  if(error) {
+      db.run(query, [link, "{}", status, srLink, score, comments], function(err) {
+        console.log("Still error inserting");
+    });
+  }
 }
 
 async function linkExists(url) {
@@ -133,9 +222,6 @@ async function getRedditList(url) {
     //Get reddit link
     let redditLink = "https://reddit.com" + redditThread.data.permalink;
     
-
-
-
     //Sort by controversial
     redditLink = redditLink.substr(0, redditLink.length-1) + ".json?sort=controversial";
 
---
+++ submitpost.py
@@ -0,0 +1,6 @@
+import requests 
+
+
+URL = "http://localhost:3110/postthread"
+r = requests.post(url = URL) 
+print(r)
